1c31cba106df99370bc58bfd8fd968a49a421bb6
aamole <amowallet@gmail.com>
2025-03-04 23:25:30 +0300

    update

--- fragpy/methods/auth/send_confirm.py
+++ fragpy/methods/auth/send_confirm.py
@@ -2,16 +2,22 @@ import logging
 import fragpy
 from fragpy.methods.api import send
 
-log = logging.getLogger(__name__) 
+log = logging.getLogger(__name__)
 
 class SendConfirm:
-    async def send_confirm(self: "fragpy.Client",phone_number: str):
-        """Send confirm message to the given phone number.
-        """
+    async def send_confirm(self: "fragpy.Client", phone_number: str):
+        """Send confirmation message to the given phone number."""
+        
         self.phone_number = phone_number
-        print(self.phone_number)
+        print(f"Sending confirmation to: {self.phone_number}")
+
         _, response = await send('GET', fragpy.fram(''))
-        self.stel_ssid = response.cookies['stel_ssid'].value
+        self.stel_ssid = response.cookies.get('stel_ssid', None)
+
+        if not self.stel_ssid:
+            log.error("Failed to retrieve stel_ssid.")
+            raise ConnectionError('Failed to retrieve stel_ssid.')
+
         data, response = await send(
             'POST', 
             url=fragpy.oauth('/auth/request'), 
@@ -20,11 +26,13 @@ class SendConfirm:
             headers=fragpy.headers,
             params=fragpy.params
         )
-        if data:
+
+        if isinstance(data, str):
             if data == 'Invalid phone number':
-                log.error("Invalid phone number %s change it.", self.phone_number)
-            if data == 'false':
-                pass
-            
-            print(data)
-            print(response)
+                log.error("Invalid phone number: %s. Change it.", self.phone_number)
+                
+        elif isinstance(data, bool) and not data:
+            log.warning("Received a False response for phone: %s", self.phone_number)
+        
+        print("Response data:", data)
+        print("Full response:", response)
